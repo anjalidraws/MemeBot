557e56860e8caa83dfa6d064fc70cff1fbfc21f0
Abhishek Verma <abhishek201404@axiscolleges.in>
2022-10-31 12:09:21 +0530

    Added Meme API

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
 import telebot
+import requests
+bot = telebot.TeleBot("bot_token")
+
+@bot.message_handler(commands=["meme"])
+def meme(message):
+    api_url = "https://meme-api.herokuapp.com/gimme"
+    response = requests.get(api_url).json()
+    bot.send_photo(message.chat.id, response["url"])
+
+bot.polling()
